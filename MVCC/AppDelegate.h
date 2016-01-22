34ba93104568e7fed54d72495ad661cd3bbd01ed
James Tang <j@jamztang.com>
2016-01-23 01:36:16 +0800

    Initial Commit

---
+++ MVCC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVCC
+//
+//  Created by James Tang on 23/1/2016.
+//  Copyright © 2016 James Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVCC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MVCC
+//
+//  Created by James Tang on 23/1/2016.
+//  Copyright © 2016 James Tang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
